c2f31c40d0d3c1a5d3eb8d08169b764e12fdf780
EthanBostick <ethan.bostick@gmail.com>
2026-07-09 20:29:30 -0400

    init structure setup

--- core/src/main/java/io/github/ethanBostick/Main.java
+++ core/src/main/java/io/github/ethanBostick/Main.java
@@ -1,27 +1,48 @@
 package io.github.ethanBostick;
 
+// Input
+import io.github.ethanBostick.input.InputRouter;
+import io.github.ethanBostick.input.MenuInputAdapter;
+import io.github.ethanBostick.input.MapInputAdapter;
+import io.github.ethanBostick.input.PopupInputAdapter;
+// UI
+import io.github.ethanBostick.ui.GameHUD;
+
+//GDX stuff
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
+public class Main extends Game {
     private SpriteBatch batch;
     private Texture image;
+
+	//temp testing
+	private Stage stage;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
         image = new Texture("libgdx.png");
+
+		//init basic input router
+		InputRouter theInputRouter = new InputRouter(new MenuInputAdapter());
+		Gdx.input.setInputProcessor(theInputRouter.getMultiplexer());
+
+		//init ui systems
+		GameHUD theGameHUD = new GameHUD();
+		theInputRouter.addProcessorAt(0,theGameHUD.stage);
+		this.stage = theGameHUD.stage;
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+		super.render();
     }
 
     @Override
---
+++ core/src/main/java/io/github/ethanBostick/input/MenuInputAdapter.java
@@ -0,0 +1,61 @@
+package io.github.ethanBostick.input;
+
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.Input;
+
+public class MenuInputAdapter extends InputAdapter{
+
+	//reads mouse clicks
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		System.out.printf("%d,%d\n",screenX,screenY);
+		System.out.printf("b: %d\n",button);
+
+		switch(button){
+			case Input.Buttons.LEFT:
+				System.out.printf("left-click\n");
+				break;
+			case Input.Buttons.RIGHT:
+				System.out.printf("right-click\n");
+				break;
+		}
+
+		return true;
+	}
+
+	//key read method
+	@Override
+	public boolean keyDown(int keycode){
+
+		switch(keycode){
+			case Input.Keys.W:
+				System.out.println("W");
+				break;
+			case Input.Keys.S:
+				System.out.println("S");
+				break;
+			case Input.Keys.A:
+				System.out.println("A");
+				break;
+			case Input.Keys.D:
+				System.out.println("D");
+				break;
+		}
+		return true;
+	}
+
+	//scroll wheel reading
+	@Override
+	public boolean scrolled(float amountX, float amountY){
+
+		if (amountY > 0){
+			System.out.printf("scrolled %f\n",amountY);
+			return true;
+		}
+		else if (amountY < 0){
+			System.out.printf("scrolled %f\n",amountY);
+			return true;
+		}
+		return false;
+	}
+}
